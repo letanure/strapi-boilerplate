5b5e181dace16fc0e92ca36fdfbeaf3af6fb6378
Luiz Tanure <letanure@gmail.com>
2023-08-31 21:53:35 +0200

    refactor: Remove landing-page schema, controller, router, and service

--- src/api/landing-page/controllers/landing-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * landing-page controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::landing-page.landing-page');
--- src/api/landing-page/services/landing-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * landing-page service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::landing-page.landing-page');
