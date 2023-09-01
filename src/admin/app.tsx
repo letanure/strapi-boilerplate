600aa9f97111af3a6ed373634c5e72787a8ad503
Luiz Tanure <letanure@gmail.com>
2023-09-01 09:40:06 +0200

    style: remove commented out code and clean up formatting

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,34 +1,22 @@
 export default {
   config: {
-    locales: [
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      // 'es',
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      // 'tr',
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
+    // Override or extend the theme
+    theme: {
+      colors: {
+        primary100: "#f6ecfc",
+        primary200: "#e0c1f4",
+        primary500: "#ac73e6",
+        primary600: "#9736e8",
+        primary700: "#8312d1",
+        danger700: "#b72b1a",
+      },
+    },
+    // Disable video tutorials
+    tutorials: false,
+    // Disable notifications about new Strapi releases
+    notifications: { releases: false },
   },
+
   bootstrap(app) {
     console.log(app);
   },
